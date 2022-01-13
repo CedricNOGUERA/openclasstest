b5a61865bd26408dd2a8dd5df144041cfc36ebdf
CedricNOGUERA <cedric.noguera@lwane.com>
2022-01-12 16:47:32 -1000

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import "../styles/Banner.css";
+
+function Banner() {
+	const title = "La maison du jouir";
+	return (
+		<div className="lmj-banner mb-2">
+			<h1>{title}</h1>
+		</div>
+	);
+}
+
+export default Banner;
---
+++ src/components/Cart.js
@@ -0,0 +1,29 @@
+// Composant Card
+
+function Cart() {
+	var PxMonstera = 8,
+		PxLierre = 10,
+		PxBqtFleur = 15;
+
+	return (
+		<div className="col-sm-4">
+			<div className="card">
+				<div className="card-body">
+					<h2> Panier </h2>
+					<ul className="list-group">
+						<li className="list-group-item">Monsetra : {PxMonstera} €</li>
+						<li className="list-group-item">Lierre : {PxLierre} €</li>
+						<li className="list-group-item">
+							Bouquet de fleur : {PxBqtFleur} €
+						</li>
+						<li className="list-group-item">{/* <Description /> */}</li>
+					</ul>
+					<br />
+					<div> Total : {PxMonstera + PxLierre + PxBqtFleur} €</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Cart;
